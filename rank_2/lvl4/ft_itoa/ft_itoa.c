cab9324c677f9efbe74bdd5035b5d006f11aa3f8
Joao <nascimento.joaogb@gmail.com>
2023-02-10 08:18:21 +0200

    ft_itoa adjustments

--- rank_2/lvl4/ft_itoa/ft_itoa.c
+++ rank_2/lvl4/ft_itoa/ft_itoa.c
@@ -6,45 +6,64 @@
 /*   By: jonascim <jonascim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/19 12:17:27 by jonascim          #+#    #+#             */
-/*   Updated: 2023/02/09 10:42:14 by jonascim         ###   ########.fr       */
+/*   Updated: 2023/02/10 08:16:54 by jonascim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
+#include <stdio.h>
 
-char	*ft_itoa(int nbr)
+int	lenght(long nbr)
 {
-	int		len;
-	long	temp;
-	char	*str;
+	int	aux;
 
-	len = 0;
-	temp = nbr;
-	str[len] = '\0';
-	if (nbr == -21478483648)
-		return ("-21478483648");
-	if (!(str = malloc(sizeof(char *) * len + 1)))
-		return (NULL);
-	if (nbr == 0)
-	{
-		str[0] = '0';
-		return (str);
-	}
+	aux = 0;
+
 	if (nbr < 0)
 	{
-		len++;
-		str[0] = '-';
+		aux++;
 		nbr *= -1;
-	}
-	while (temp)
-	{
-		temp /= 10;
-		len++;
 	}
 	while (nbr)
 	{
-		str[--len] = (nbr % 10) + '0';
+		aux++;
 		nbr /= 10;
 	}
-	return (str);
+	return (aux);
 }
+
+char	*ft_itoa(int nbr)
+{
+	char	*res;
+	long	temp;
+	int		len;
+
+	temp = nbr;
+	len = lenght(temp);
+	res = malloc(sizeof(char *) * (len + 1));
+	if (!res)
+		return (NULL);
+	res[len] = '\0';
+	if (temp < 0)
+	{
+		res[0] = '-';
+		temp *= -1;
+	}
+	if (temp == 0)
+	{
+		res[0] = '0';
+		return (res);
+	}
+	while (temp)
+	{
+		res[--len] = (temp % 10) + '0';
+		temp /= 10;
+	}
+	return (res);
+}
+
+// int	main(void)
+// {
+// 	printf("%s\n", ft_itoa(0));
+// 	return (0);
+// }
